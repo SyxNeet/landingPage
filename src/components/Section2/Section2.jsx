035169a566cba8e12b179eaf3c5d10efc39ab791
lmt19 <minhtri192002@gmail.com>
2023-08-06 15:19:41 +0700

    text slide

--- src/components/Section2/Section2.jsx
+++ src/components/Section2/Section2.jsx
@@ -23,84 +23,86 @@ import "./style.scss";
 function Section2() {
   const listImg = [img1, imgCenter, img3, img4];
   return (
-    <ContentWrapper>
-      <div className='section2'>
-        <div className='section-top'>
-          <div className='content-left'>
-            <div className='about-us'>
-              <p>ABOUT US</p>
-              <p>
-                <span> Offshore Energy Installation </span>
-                <span>OEI is your gateway to renewable energy and</span>
-                <span> Oil</span> & <span>Gas</span> solutions.
-              </p>
-            </div>
-
-            <div className='seeMore'>
-              <span>SEE MORE</span>
-            </div>
-          </div>
-          <div className='content-right'>
-            <div className='flex-container'>
-              <div className='flex-left'>
-                <div className='flex-left-top'>
-                  <div className='item3'>
-                    <Fade left>
-                      <img src={img1} alt='' />
-                    </Fade>
-                  </div>
-                  <div className='item7'>
-                    <Fade left>
-                      <img src={imgCenter} alt='' />
-                    </Fade>
-                  </div>
-                </div>
-                <div className='flex-left-bot'>
-                  <div className='item4'>
-                    <img src={layout2} alt="" />
-                  </div>
-                  <div className='item2'>
-                    <Fade right>
-                      <img src={img4} alt='' />
-                    </Fade>
-                  </div>
-                </div>
-              </div>
-
-              <div className='flex-right'>
-                <div className='item1'>
-                  <img src={layout1} alt="" />
-                </div>
-                <div className='item5'>
-                  <Fade right>
-                    <img src={img3} alt='' />
-                  </Fade>
-                </div>
-                <div className='item6'>
-                  <img src={layout3} alt="" />
-
-                </div>
-              </div>
-            </div>
-          </div>
-
-
-        </div>
-
-        <div className='section-bot'>
-          <Ticker>
-            {() => (
-              <>
+    <>
+      <ContentWrapper>
+        <div className='section2'>
+          <div className='section-top'>
+            <div className='content-left'>
+              <div className='about-us'>
+                <p>ABOUT US</p>
                 <p>
-                  offshore <span> Energy </span> installation
+                  <span> Offshore Energy Installation </span>
+                  <span>OEI is your gateway to renewable energy and</span>
+                  <span> Oil</span> & <span>Gas</span> solutions.
                 </p>
-              </>
-            )}
-          </Ticker>
+              </div>
 
+              <div className='seeMore'>
+                <span>SEE MORE</span>
+              </div>
+            </div>
+            <div className='content-right'>
+              <div className='flex-container'>
+                <div className='flex-left'>
+                  <div className='flex-left-top'>
+                    <div className='item3'>
+                      <Fade left>
+                        <img src={img1} alt='' />
+                      </Fade>
+                    </div>
+                    <div className='item7'>
+                      <Fade left>
+                        <img src={imgCenter} alt='' />
+                      </Fade>
+                    </div>
+                  </div>
+                  <div className='flex-left-bot'>
+                    <div className='item4'>
+                      <img src={layout2} alt="" />
+                    </div>
+                    <div className='item2'>
+                      <Fade right>
+                        <img src={img4} alt='' />
+                      </Fade>
+                    </div>
+                  </div>
+                </div>
+
+                <div className='flex-right'>
+                  <div className='item1'>
+                    <img src={layout1} alt="" />
+                  </div>
+                  <div className='item5'>
+                    <Fade right>
+                      <img src={img3} alt='' />
+                    </Fade>
+                  </div>
+                  <div className='item6'>
+                    <img src={layout3} alt="" />
+
+                  </div>
+                </div>
+              </div>
+            </div>
+
+
+          </div>
+
+          <div className='section-bot'>
+            <Ticker>
+              {() => (
+                <>
+                  <p>
+                    offshore <span> Energy </span> installation
+                  </p>
+                </>
+              )}
+            </Ticker>
+
+          </div>
         </div>
-      </div>
-    </ContentWrapper>
+      </ContentWrapper>
+    </>
   );
 }
 
